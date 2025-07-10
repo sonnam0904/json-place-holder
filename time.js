7e6cbf674e37671d5343a5bd1afbf98ea8cd2e5b
Nguyễn Nam Sơn <sonnn.uct@gmail.com>
2025-07-10 17:09:06 +0700

    Update time.js

--- time.js
+++ time.js
@@ -8,25 +8,13 @@
     {
       "code": "GGG001",
       "name": "GGG Steak House - Hà Nội",
-      "address": "123 Trần Hưng Đạo, Hoàn Kiếm, Hà Nội",
+      "address": "17T4 Hoàng Đạo Thúy - Nhân Chính - Thanh Xuân - Hà Nội",
       "telephone": "024-1234-5678",
       "schedules": [
         "10:00",
         "11:30",
         "13:00",
         "18:00"
-      ]
-    },
-    {
-      "code": "GGG002",
-      "name": "GGG Sushi - Hà Nội",
-      "address": "456 Lý Thường Kiệt, Hai Bà Trưng, Hà Nội",
-      "telephone": "024-8765-4321",
-      "schedules": [
-        "10:00",
-        "12:00",
-        "14:00",
-        "19:30"
       ]
     }
   ]
